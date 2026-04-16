bd8c881a2cc9411d50a473358ef04738cb28d303
Mahbub Hossen <mahbubhossenpg@gmail.com>
2026-04-16 19:32:59 +0600

    google signin done

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,18 +1,21 @@
 import { betterAuth } from "better-auth";
 // If your Prisma file is located elsewhere, you can change the path
-import { prisma } from "./prisma";
 import { prismaAdapter } from "better-auth/adapters/prisma";
-
+import { prisma } from "./prisma";
 
 export const auth = betterAuth({
   database: prismaAdapter(prisma, {
     provider: "postgresql",
-    
   }),
-  
+
   trustedOrigins: [process.env.APP_URL!, "http://localhost:3000"],
   emailAndPassword: {
     enabled: true,
-    
+  },
+  socialProviders: {
+    google: {
+      clientId: process.env.GOOGLE_CLIENT_ID as string,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+    },
   },
 });
