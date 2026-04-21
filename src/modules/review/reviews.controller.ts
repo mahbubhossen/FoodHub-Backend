b12732dcad88242a00d6285367b8e5a852c1ed1f
Mahbub Hossen <mahbubhossenpg@gmail.com>
2026-04-21 17:10:49 +0600

    review controller done

---
+++ src/modules/review/reviews.controller.ts
@@ -0,0 +1,74 @@
+import { NextFunction, Request, Response } from "express";
+import { UserRole } from "../../middlewares/auth";
+import { reviewService } from "./reviews.service";
+
+const createReview = async (
+  req: Request,
+  res: Response,
+  next: NextFunction,
+) => {
+  try {
+    const result = await reviewService.createReview(req.user!.id, req.body);
+    res.status(201).json(result);
+  } catch (error) {
+    next(error);
+  }
+};
+
+const getMealReviews = async (
+  req: Request,
+  res: Response,
+  next: NextFunction,
+) => {
+  try {
+    const page = parseInt(req.query.page as string) || 1;
+    const limit = parseInt(req.query.limit as string) || 10;
+
+    const result = await reviewService.getMealReviews(req.params.mealId, {
+      page,
+      limit,
+      skip: (page - 1) * limit,
+    });
+    res.status(200).json(result);
+  } catch (error) {
+    next(error);
+  }
+};
+
+const getMyReviews = async (
+  req: Request,
+  res: Response,
+  next: NextFunction,
+) => {
+  try {
+    const result = await reviewService.getMyReviews(req.user!.id);
+    res.status(200).json(result);
+  } catch (error) {
+    next(error);
+  }
+};
+
+const deleteReview = async (
+  req: Request,
+  res: Response,
+  next: NextFunction,
+) => {
+  try {
+    const isAdmin = req.user!.role === UserRole.ADMIN;
+    await reviewService.deleteReview(
+      req.params.reviewId,
+      req.user!.id,
+      isAdmin,
+    );
+    res.status(200).json({ message: "Review deleted successfully." });
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const ReviewController = {
+  createReview,
+  getMealReviews,
+  getMyReviews,
+  deleteReview,
+};
