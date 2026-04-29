56323087b8de32929ae126455816e5697a53e602
Mahbub Hossen <mahbubhossenpg@gmail.com>
2026-04-29 19:43:45 +0600

    server .js fixed

--- src/server.ts
+++ src/server.ts
@@ -3,22 +3,19 @@ import { prisma } from "./lib/prisma";
 
 const PORT = process.env.PORT || 5000;
 
-// Only run server locally (NOT on Vercel)
-if (process.env.NODE_ENV !== "production") {
-  async function main() {
-    try {
-      await prisma.$connect();
-      console.log("Connected to the database");
+async function main() {
+  try {
+    await prisma.$connect();
+    console.log("Connected to the database");
 
-      app.listen(PORT, () => {
-        console.log(` Server running on http://localhost:${PORT}`);
-      });
-    } catch (error) {
-      console.error("An error occurred", error);
-      await prisma.$disconnect();
-      process.exit(1);
-    }
+    app.listen(PORT, () => {
+      console.log(`Server running on port ${PORT}`);
+    });
+  } catch (error) {
+    console.error("Server failed to start:", error);
+    await prisma.$disconnect();
+    process.exit(1);
   }
-
-  main();
 }
+
+main();
